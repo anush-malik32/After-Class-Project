d9a9cfd8e019cf456b146f70010f222387472ce3
anush-malik32 <malik.anush1@gmail.com>
2026-07-18 15:48:29 -0400

    today i worked on storing birthday's

---
+++ Python/birthdays.py
@@ -0,0 +1,6 @@
+Kyle = "May 5th 2014"
+Isaac = "August 21st 2014"
+Johan = "December 20th 2013"
+Rishaan = "October 11th 2014"
+Arjun = "March 27th 2014"
+print(f"In order Arjun's birthday is {Arjun}, Kyle's birthday is {Kyle}, Issac's birthday is {Isaac}, Rishaan's birthday is {Rishaan}, and Johan's birthday is {Johan}.")
